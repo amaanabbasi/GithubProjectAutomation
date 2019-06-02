4d0f3e8a5d396c21dbea95d374a74b039f8d4674
amaanabbasi <amaanabbasi99@gmail.com>
2019-06-02 10:15:18 +0530

    sd

---
+++ create.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Setting Up," $1
+python3 create.py $1
+echo "--------Repository Created ---------"
+cd code/
+echo " "
+echo "Setting up git locally"
+mkdir $1
+cd $1
+git init .
+git remote add origin https://github.com/yourusername/$1.git
+touch README.md
+git add .
+git commit -m "Initial commit"
+git push -u origin master
+echo " "
+echo "--------Finished---------"
+echo " "
+echo "Launching Vs code"
+code . 
